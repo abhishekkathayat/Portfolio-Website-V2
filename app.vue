7e9e0950984a5b34f38dab5b8b52deee3d0594a6
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2023-05-12 11:11:31 +0530

    Minor Changes to Intro and App Layouts

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container font-inter text-white h-screen max-w-full flex flex-col lg:flex-row bg-bgclr">
+    <div class="font-inter text-white h-screen flex flex-col lg:flex-row bg-bgclr">
 		<div class="content-left w-full lg:w-3/6">
 			<Intro />
 		</div>
--- components/Intro.vue
+++ components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col flex-grow-1 h-full pl-24 py-20">
+    <div class="flex flex-col flex-grow-1 h-full pl-36 py-20">
         <h1 class="font-bold text-5xl leading-tight"> Abhishek Kathayat </h1>
         <h2 class="font-semibold text-[22px]"> Specialist Programmer at Infosys </h2>
         <p class="mt-10 text-sm text-subclr leading-relaxed"> 
@@ -21,7 +21,7 @@
         <div class="text-sm mt-auto">
             <ul class="flex flex-row">
                 <li class="flex flex-row items-center mr-8" v-for="(externalLink, index) in externalLinks" :key="index">
-                    <img class="mx-3" :src="externalLink.img" height="20" width="20"/>
+                    <img class="mr-3" :src="externalLink.img" height="20" width="20"/>
                     {{ externalLink.name }}
                     <a :href="externalLink.url"> 
                         <img class="mx-2" src="../assets/icons/external.svg" height="12" width="12"/>
