3876ca1a64fc1416aec8952b65117984483426ba
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2023-05-14 12:59:47 +0530

    Articles Update
    Formatting Published Date to show only Date in DD-MM-YYYY Format.
    Removing Time from Date.

--- components/Articles.vue
+++ components/Articles.vue
@@ -4,7 +4,7 @@
             v-for="(article, index) of articles" :key="index" :style="{ 'background-image': `url(${article.thumbnail})` }">
             <div class="absolute inset-0 bg-gradient-to-r from-bgclr/100 to-bgclr/90"></div>
             <div class="flex flex-col z-10">
-                <div class="text-[11px] tracking-[2px] uppercase"> {{ article.pubDate }} </div>
+                <div class="text-[11px] tracking-[2px] uppercase"> {{ formatPublishDate(article.pubDate) }} </div>
                 <h1 class="mt-2 font-semibold text-lg leading-tight"> {{ article.title }} </h1>
                 <ul class="mt-4 flex whitespace-nowrap flex-wrap">
                     <li class="text-[10px] bg-topicsbgclr rounded-full font-semibold text-white px-2 py-1 mr-2 mb-2"
@@ -28,5 +28,9 @@
 
 const articles: Array<any> = await useFetch('/api/medium')
     .then(response => response.data.value.items);
+
+function formatPublishDate(date: string): string {
+    return date.split(" ")[0].split("-").reverse().join("-");
+}
 
 </script>
