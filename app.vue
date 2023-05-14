9552c291b0ec6942c29a6572c06ef0a7a8526b87
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2023-05-14 17:55:53 +0530

    UI Changes
    Renamed Articles to Article.
    Added href to jump to section on navigation links clicked.
    Added Ids to Right Side Content.

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@
 		<div class="content-left w-full lg:w-3/6">
 			<Intro />
 		</div>
-		<div class="content-right w-full lg:w-3/6 overflow-y-auto">
-			<Experience />
-			<Project class="mt-12" />
-			<Articles class="mt-12 mb-12" />
+		<div class="content-right w-full lg:w-3/6 overflow-y-auto scroll-smooth">
+			<Experience id="experience" />
+			<Project id="projects" class="mt-12" />
+			<Articles id="articles" class="mt-12 mb-12" />
 		</div>
   	</div>
 </template>
