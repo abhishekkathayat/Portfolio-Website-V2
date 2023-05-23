bf8b5a680888737ab6ee103d1a574d7a2a83e815
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2023-05-23 07:18:50 +0530

    UI Updates
    Margin Updates for Project, Experience and Article cards.
    Width Fit update for Article and Experience.

--- components/Article.vue
+++ components/Article.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col mx-6 sm:mx-10 lg:mx-0">
         <div class="lg:hidden text-xs uppercase font-semibold tracking-[2px] mb-6"> Articles </div>
         <div class="flex flex-col lg:w-fit group">
-            <div class="lg:w-[460px] xl:w-[580px] 2xl:w-[700px] flex flex-col p-6 sm:p-8 mb-6 relative bg-center
+            <div class="lg:w-[460px] xl:w-[580px] 2xl:w-[700px] flex flex-col p-6 sm:p-8 mb-4 relative bg-center
                     lg:hover:scale-105 lg:duration-200 lg:group-hover:opacity-50 lg:hover:!opacity-100"
                 v-for="(article, index) of articles" :key="index" :style="{ 'background-image': `url(${article.thumbnail})` }">
                 <div class="absolute inset-0 bg-gradient-to-r from-bgclr/100 to-bgclr/90"></div>
@@ -25,7 +25,7 @@
                 </div>
             </div>
         </div>
-        <div class="w-fit">
+        <div class="w-fit mt-2">
             <a class="flex items-center group" :href="props.platforms && props.platforms[2].url" target="_blank" rel="noopener noreferrer">
                 <span class="font-semibold tracking-wide text-sm"> View all Articles </span>
                 <img class="ml-2 group-hover:ml-4 duration-300" src="~/assets/icons/right-arrow.svg" height="20" width="20"/>
--- components/Experience.vue
+++ components/Experience.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col mx-6 sm:mx-10 lg:mx-0">
         <div class="lg:hidden text-xs uppercase font-semibold tracking-[2px] mb-6"> Experience </div>
-        <div class="flex flex-col w-fit group">
-            <div class="lg:w-[460px] xl:w-[580px] 2xl:w-[700px] bg-cardbgclr p-6 sm:p-8 mb-6 
+        <div class="flex flex-col lg:w-fit group">
+            <div class="lg:w-[460px] xl:w-[580px] 2xl:w-[700px] bg-cardbgclr p-6 sm:p-8 mb-4
                     lg:hover:scale-105 lg:duration-200 lg:group-hover:opacity-50 lg:hover:!opacity-100"
                 v-for="(experience, index) in props.experience" :key="index">
                 <div class="text-[11px] tracking-[2px]"> {{ experience.from }} &mdash; {{ experience.to }} </div>
@@ -20,7 +20,7 @@
                 </ul>
             </div>
         </div>
-        <div class="w-fit">
+        <div class="w-fit mt-2">
             <a class="flex items-center group" :href="props.platforms && props.platforms[0].url" target="_blank" rel="noopener noreferrer">
                 <span class="font-semibold tracking-wide text-sm"> View LinkedIn Profile </span>
                 <img class="ml-2 group-hover:ml-4 duration-300" src="~/assets/icons/right-arrow.svg" height="20" width="20"/>
